207de561c5372e2b8499d8b71b0740eb64521f50
jeancarlo89 <jeancarlodev@gmail.com>
2025-11-14 19:34:35 -0300

    fix: add dist to gitignore

--- dist/src/core/infra/databases/Client.in-memory.repo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ClientInMemoryRepository = void 0;
-const in_memory_repo_1 = require("../../shared/infra/in-memory.repo");
-class ClientInMemoryRepository extends in_memory_repo_1.InMemoryRepository {
-}
-exports.ClientInMemoryRepository = ClientInMemoryRepository;
-//# sourceMappingURL=Client.in-memory.repo.js.map
